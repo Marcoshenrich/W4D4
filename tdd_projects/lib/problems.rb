a58ee96d285daea7e1f23bceeb9f9a61ecff3a02
remnantsofred <remnantsofred@gmail.com>
2022-10-27 14:19:21 -0700

    two_sum

--- tdd_projects/lib/problems.rb
+++ tdd_projects/lib/problems.rb
@@ -7,4 +7,17 @@ class Array
         end
         resarr
     end
+
+    def two_sum
+        resarr = []
+        self.each_with_index do | num_1, i |
+            self.each_with_index do | num_2, j |
+                if num_1 + num_2 == 0 && j > i 
+                    resarr << [i, j]
+                end
+            end 
+        end
+        resarr
+    end
+
 end
--- tdd_projects/spec/problems_spec.rb
+++ tdd_projects/spec/problems_spec.rb
@@ -3,18 +3,28 @@ require "problems"
 
 describe "Array" do
     describe "my_uniq" do
+        subject(:arr) { [1, 2, 1, 3, 3] }
+
         it "returns a different array" do
-            arr = [1, 2, 1, 3, 3]
+            # arr = [1, 2, 1, 3, 3]
             expect(arr.my_uniq.object_id).not_to be(arr.object_id)
         end
 
         it "removes duplicates from the array" do
-            arr = [1, 2, 1, 3, 3]
+            # arr = [1, 2, 1, 3, 3]
             expect(arr.my_uniq).to eq([1, 2, 3])
         end
     end
 
-
+    describe "two_sum" do
+        it "finds all pairs of positions that sum to zero" do
+            arr = [-1, 0, 2, -2, 1]
+            expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+        end
+        #[-1, 0, 2, -2, 1].two_sum # => [[0, 4], [2, 3]]
+        #[0, 2] before [2, 1] (smaller first elements come first)
+        #[0, 1] before [0, 2] (then smaller second elements come first)
+    end
 
 
 
